d21d5a505b049108d149d25da9469ba4569c0a92
Conphucious <9phuc.nguyen6@gmail.com>
2020-02-02 01:57:10 -0600

    initial commit

---
+++ src/main/java/com/ngu_software/wbdb/App.java
@@ -0,0 +1,13 @@
+package com.ngu_software.wbdb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
